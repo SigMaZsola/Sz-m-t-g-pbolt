66e8350c1e5c1dc9e66c4608c22fa8b435ac7bfd
BiRaw <biropeter2008@gmail.com>
2024-11-21 11:13:41 +0100

    -First half of file reading, -Responsibility added, -Sidebar and searchbar is scrolling with the screen.

--- main_page/script.js
+++ main_page/script.js
@@ -1,1 +1,70 @@
-var fs = require('fs');
+fetch("src/src.txt")
+    .then((res) => res.text())
+    .then((text) => {createItems(text)})
+    .catch((e) => console.error(e));
+
+const item_container = document.getElementById("items")
+
+function createItems(text){
+    let rows = text.split("\n")
+    rows.forEach((line) => {createItem(line)})
+}
+
+function createItem(line){
+    let split_txt = line.toString().split(";")
+
+    let parts = {
+        id: split_txt[0],
+        type: split_txt[1],
+        brand: split_txt[2],
+        model: split_txt[3],
+        price: split_txt[4]
+    }
+
+    let item = document.createElement("div")
+    item.classList.add("item","element")
+    item_container.appendChild(item)
+
+    let item_img = document.createElement("div")
+    item_img.classList.add("item-img")
+    item.appendChild(item_img)
+
+    let item_description = document.createElement("div")
+    item_description.classList.add("item-description")
+    item_description.innerText = `
+            Type: ${parts["type"]}
+            Brand: ${parts["brand"]}
+            Model: ${parts["model"]}
+            Price: ${parts["price"]}
+            a
+            a
+            a
+            a
+            a
+            a
+            a
+            a
+        `
+    item.appendChild(item_description)
+
+    let plus = document.createElement("div")
+    plus.classList.add("plus")
+    plus.innerText = "+"
+    item.appendChild(plus)
+}
+
+// Making the sidebar scroll
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector('.side-bar');
+    const initialOffsetTop = sidebar.offsetTop;
+
+    window.addEventListener('scroll', function() {
+        const scrollDistance = window.pageYOffset;
+        const newTop = initialOffsetTop + scrollDistance;
+
+        sidebar.style.top = `${newTop}px`;
+    });
+});
+
